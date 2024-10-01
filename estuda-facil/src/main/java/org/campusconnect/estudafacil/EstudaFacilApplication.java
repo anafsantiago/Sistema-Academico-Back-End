24e1ac8eae66458ea8ad6e7e36c18cab7e8a9e59
Joas França dos Santos <joasfsantosti@gmail.com>
2024-09-30 23:59:37 -0300

    GitIgnore e configurações maven.

---
+++ estuda-facil/src/main/java/org/campusconnect/estudafacil/EstudaFacilApplication.java
@@ -0,0 +1,13 @@
+package org.campusconnect.estudafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudaFacilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudaFacilApplication.class, args);
+    }
+
+}
---
+++ estuda-facil/src/test/java/org/campusconnect/estudafacil/EstudaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package org.campusconnect.estudafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudaFacilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
